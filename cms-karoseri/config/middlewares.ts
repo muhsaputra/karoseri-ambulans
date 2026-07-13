cab8af1297363e6cc638c4284375b29c9afb3a09
muhsaputra <muhsaputrabiz@gmail.com>
2026-07-13 09:08:12 +0700

    update all

--- cms-karoseri/config/middlewares.ts
+++ cms-karoseri/config/middlewares.ts
@@ -1,16 +1,17 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+// test
 
 export default config;
